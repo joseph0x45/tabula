6a415d6747af36bced80fc5e0181a4921a4f845e
TheWisePigeon <zozozozeph@gmail.com>
2023-03-07 16:20:40 +0000

    feat: added dark mode support

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,39 +16,41 @@
     })
 </script>
 
-<main class=" p-1 relative h-screen w-full " >
+<main class=" p-1 relative h-screen w-full dark:text-slate-300 " >
     <ToolTip/>
     <Socials/>
     {#if !loading && loaded_data}
 
         {#if loaded_data.tasks.length===0}
 
-            <div class=" text-gray-500 h-full w-fit  m-auto flex flex-col items-center justify-center gap-1 "  >
+            <div class=" text-gray-500 dark:text-gray-300 h-full w-fit  m-auto flex flex-col items-center justify-center gap-1 "  >
                 <div class=" flex items-center justify-center w-full" >
                     <img src="icon.png" width="100px" alt="" srcset="">
                     <h1 class=" font-bold text-3xl" >Tabula <sup class=" text-sm" >Prima </sup></h1>
                 </div>
                 <h1 class=" font-bold " >Tabula is your place to organize your tasks with ease</h1>
                 <div class=" flex flex-col w-[70%] gap-2 text-sm font-thin mt-5" >
-                    <button class="flex items-center gap-2 text-start w-full p-2 hover:bg-gray-200 hover:text-gray-700 rounded-md " >
+                    <button 
+                        class="welcome_screen_button" 
+                    >
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
                             <path d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
                           </svg>                          
                         <h1>New Task</h1>
                     </button>
-                    <button title="Coming soon" class=" flex items-center gap-2 text-start p-2 w-full hover:bg-gray-200 hover:text-gray-700 rounded-md cursor-not-allowed " >
+                    <button title="Coming soon" class="welcome_screen_button cursor-not-allowed " >
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
                             <path fill-rule="evenodd" d="M13.887 3.182c.396.037.79.08 1.183.128C16.194 3.45 17 4.414 17 5.517V16.75A2.25 2.25 0 0114.75 19h-9.5A2.25 2.25 0 013 16.75V5.517c0-1.103.806-2.068 1.93-2.207.393-.048.787-.09 1.183-.128A3.001 3.001 0 019 1h2c1.373 0 2.531.923 2.887 2.182zM7.5 4A1.5 1.5 0 019 2.5h2A1.5 1.5 0 0112.5 4v.5h-5V4z" clip-rule="evenodd" />
                         </svg>
                         <h1>New Board</h1>
                     </button>
-                    <button class=" flex items-center gap-2 text-start p-2 w-full hover:bg-gray-200 hover:text-gray-700 rounded-md " >
+                    <button class="welcome_screen_button" >
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
                             <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a.75.75 0 000 1.5h.253a.25.25 0 01.244.304l-.459 2.066A1.75 1.75 0 0010.747 15H11a.75.75 0 000-1.5h-.253a.25.25 0 01-.244-.304l.459-2.066A1.75 1.75 0 009.253 9H9z" clip-rule="evenodd" />
                         </svg>                          
                         Shortcuts and Tips
                     </button>
-                    <a href="https://tabula.lol" class=" flex items-center gap-2 text-start p-2 w-full hover:bg-gray-200 rounded-md " >
+                    <a href="https://tabula.lol" class="welcome_screen_button " >
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
                             <path fill-rule="evenodd" d="M4.25 5.5a.75.75 0 00-.75.75v8.5c0 .414.336.75.75.75h8.5a.75.75 0 00.75-.75v-4a.75.75 0 011.5 0v4A2.25 2.25 0 0112.75 17h-8.5A2.25 2.25 0 012 14.75v-8.5A2.25 2.25 0 014.25 4h5a.75.75 0 010 1.5h-5z" clip-rule="evenodd" />
                             <path fill-rule="evenodd" d="M6.194 12.753a.75.75 0 001.06.053L16.5 4.44v2.81a.75.75 0 001.5 0v-4.5a.75.75 0 00-.75-.75h-4.5a.75.75 0 000 1.5h2.553l-9.056 8.194a.75.75 0 00-.053 1.06z" clip-rule="evenodd" />
