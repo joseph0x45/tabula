8d1c98d8f46ee16c9a6b6b56b707f04e5a2171cb
TheWisePigeon <zozozozeph@gmail.com>
2023-03-14 22:50:01 +0000

    fix: create task menu showing up when pressing t inside todos

--- src/components/Todo.svelte
+++ src/components/Todo.svelte
@@ -1,5 +1,6 @@
 <script lang="ts" >
     import type { Todo } from "../types";
+    import { idling } from "../store"
     import { toggle_todo_done, update_todo_name, remove_todo } from "../store/board"
     export let todo: Todo
     export let parent_color: string
@@ -9,7 +10,7 @@
 
 <main title={todo.name} style={`background-color:${parent_color};`} class={` w-full items-center flex justify-between rounded-md p-2`} >
     <form  >
-        <input  bind:value={todo.name} type="text" class={` caret-white hover:cursor-text bg-transparent focus:outline-none w-[95%] overflow-x-clip ${todo.done?"line-through":""} `} />
+        <input on:focus={()=>{ idling.set(false) }} on:blur={()=>{ idling.set(true) }}  bind:value={todo.name} type="text" class={` caret-white hover:cursor-text bg-transparent focus:outline-none w-[95%] overflow-x-clip ${todo.done?"line-through":""} `} />
     </form>
-    <input checked={todo.done} on:change={()=>{ toggle_todo_done(parent_id, todo.id) }} type="checkbox" name="" id="">
+    <input  on:focus={()=>{ idling.set(false) }} on:blur={()=>{ idling.set(true) }}  checked={todo.done} on:change={()=>{ toggle_todo_done(parent_id, todo.id) }} type="checkbox" name="" id="">
 </main>
